55de18b919700b54b1e6ee5f5b97216ad56b0019
jrwall94 <jrwall94@gmail.com>
2025-09-11 17:31:31 -0600

    First commit

---
+++ demo0.py
@@ -0,0 +1,11 @@
+x = 5    #Assignment statement: (int)
+         #CPU, allocate a piece of memory, call it x, put the value 3 in it
+y = 7.5  #Float
+msg = "Hey" #String
+name = "Jon"  #String
+awake = True  #Boolean
+asleep = False #Boolean
+
+print(x) #Function call
+print(f"{msg} {name}! {x} plus {y} equals {x+y}.")
+print(awake)
