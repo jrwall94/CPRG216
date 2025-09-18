bd210c82ab9ffae196037f5151f13604b395e8f1
jrwall94 <jrwall94@gmail.com>
2025-09-18 16:05:16 -0600

    Revised week 3 demo

--- week3.py
+++ week3.py
@@ -20,3 +20,20 @@ print(awake, type(awake))
 print(x+y, type(x+y))
 print(type(type(x)))
 print(type(print()))
+
+# Some function calls: print, input, int, float, str, bool
+
+num_as_text = "43"
+num_as_num = int(num_as_text) # Casting string to int
+
+print(num_as_text) # Will print as text
+print(num_as_num + 5) # Prints because python uses str() function behind the scenes
+
+num = 3
+num_f = float(3)
+
+num2 = 3.4
+num2_i = int(num2)
+num2_as_text = str(num2)
+
+print(num2_as_text)
