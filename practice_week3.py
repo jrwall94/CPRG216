a7953dd5281e7707aaff12c7e5d68603a882d997
jrwall94 <jrwall94@gmail.com>
2025-09-18 16:30:11 -0600

    Fixed differing units

--- practice_week3.py
+++ practice_week3.py
@@ -1,8 +1,8 @@
 # Problem: Calculate the amount of tiles needed to cover a floor and the total cost
 
-floor_width = int(input("What is the width of your floor in feet?\n> "))
-floor_length = int(input("What is the length of your floor in feet?\n> "))
-tile_size = int(input("How many square meters are your tiles?\n> "))
+floor_width = int(input("What is the width of your floor?\n> "))
+floor_length = int(input("What is the length of your floor?\n> "))
+tile_size = int(input("How many square units are your tiles?\n> "))
 tile_price = int(input("What is the per-tile cost in dollars?\n> "))
 
 tiles = (floor_width * floor_length) / tile_size
